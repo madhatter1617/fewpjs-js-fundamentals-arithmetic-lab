ea7d4efc5f4c872c8992ceeddde58cf135cbb7d9
Rosa <rosarenee@att.net>
2021-05-17 22:09:12 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+
+const multiply = num1 * num2;
+let random = (Number.isInteger()+1)
+
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = 20;
+
